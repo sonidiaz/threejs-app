83e5c7f56e2c758ce13489107d31c283f1f461ce
sonidiaz <nachodiaz8@gmail.com>
2023-04-20 16:34:09 +0200

    feat: trigger deploy

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "310bd3ff22787572"; }
+/******/ 		__webpack_require__.h = function() { return "1c1ad47e7c002d9a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
